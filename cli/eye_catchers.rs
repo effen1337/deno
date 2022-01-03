45f18db5ab3a74fe30c20915ee17ef28e9fbac90
effen1337 <37754407+effen1337@users.noreply.github.com>
2022-01-03 20:20:24 +0100

    Create eye_catchers.rs

---
+++ cli/eye_catchers.rs
@@ -0,0 +1,18 @@
+use deno_runtime::colors;
+use log::info;
+
+pub fn check(message: &str) {
+  info!("{} {}", colors::green("Check"), message);
+}
+
+pub fn watcher(message: &str) {
+  info!("{} {}", colors::intense_blue("Watcher"), message);
+}
+
+pub fn emit(message: &str) {
+  info!("{} {}", colors::green("Emit"), message);
+}
+
+pub fn bundle(message: &str) {
+  info!("{} {}", colors::green("Bundle"), message);
+}
